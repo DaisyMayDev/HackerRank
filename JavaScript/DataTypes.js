58a5df56cee5e38a7411fc7747971e7482102b59
DaisyMayDev <49954474+DaisyMayDev@users.noreply.github.com>
2021-05-23 13:17:25 +0100

    Create DataTypes.js

--- JavaScript/DataTypes.js
+++ JavaScript/DataTypes.js
@@ -6,14 +6,16 @@ const secondDecimal = `4.32`;
 const secondString = `is the best place to learn and practice coding!`;
 
 function performOperation(secondInteger, secondDecimal, secondString) {
-    const firstInteger = 4;  
-    const firstDecimal = 4.0;
-    const firstString = 'HackerRank ';
-    
-    console.log(firstInteger + Number(secondInteger));
-    console.log(firstDecimal + Number(secondDecimal));
-    console.log(firstString + secondString);
+  const firstInteger = 4;
+  const firstDecimal = 4.0;
+  const firstString = 'HackerRank ';
+
+  console.log(firstInteger + Number(secondInteger));
+  console.log(firstDecimal + Number(secondDecimal));
+  console.log(firstString + secondString);
 }
+
+performOperation(secondInteger, secondDecimal, secondString);
 
 //Output in console ->  16
 //                      8.32
