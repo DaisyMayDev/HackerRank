3b055c40e90b54cd8c9f8f8a303325ff8091892d
DaisyMayDev <49954474+DaisyMayDev@users.noreply.github.com>
2021-05-23 13:22:19 +0100

    Create HelloWorld.js
    HackerRank 10 Days of JavaScript - Hello World Challenge

---
+++ JavaScript/HelloWorld.js
@@ -0,0 +1,14 @@
+// Hello World Challenge:
+// Print the contents of the variable 'parameterVariable' to the console.
+
+const parameterVariable = `Welcome to 10 Days of JavaScript!`;
+
+function greeting(parameterVariable) {
+  console.log('Hello, World!');
+  console.log(parameterVariable);
+}
+
+greeting(parameterVariable);
+
+//Output in console ->  Hello, World!
+//                      Welcome to 10 Days of JavaScript!
